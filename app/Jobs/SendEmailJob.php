02a1a2893218e06e7f990a2cfe9e6f11bc8079d3
Hemangi Chauhan <hemangi3317@gmail.com>
2023-03-02 09:11:44 +0530

    update

---
+++ app/Jobs/SendEmailJob.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use App\Mail\SendEmailTest;
+use Mail;
+
+class SendEmailJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $data;
+    /**
+     * Create a new job instance.
+     */
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $email = new SendEmailTest();
+        Mail::to($this->data['email'])->send($email);
+    }
+}
---
+++ resources/views/emails/test.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>How to send email usign laravel queue - Webappfix</title>
+</head>
+<body>
+	<center>
+		<h2 style="padding: 23px;background: #e2e2e2;border-bottom: 6px green solid;">
+			<a href="https://www.webappfix.com">Visit Our Website : Webappfix.com</a>
+		</h2>
+	</center>
+
+	<p>Hello, Sir</p>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+	<strong>Thank You.</strong>
+</body>
+</html>
